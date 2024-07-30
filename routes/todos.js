bf053ca4f623d0cf7d7601bb48fbd7843281b93e
Ben Manley <benjamintmanley@gmail.com>
2024-07-30 15:16:46 -0500

    rendering todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { todos } from '../data/todo-data.js'
 
 const router = Router()
 
 // GET localhost:3000/users
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('todos/index', {
+    todos
+  })
 })
 
 export { router }
