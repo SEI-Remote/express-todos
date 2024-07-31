b2f0c63e465f84afe230818b5a4dbc9a4f2700b4
Ben Manley <benjamintmanley@gmail.com>
2024-07-31 13:38:48 -0500

    implement index functionality

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,9 +1,24 @@
-import { todos } from '../data/todo-data.js'
+// Import the model that we exported in the Todo.js model file
+import { Todo } from '../models/todo.js'
 
-function index(req, res) {
-  res.render('todos/index', {
-    todos
-  })
+// This ^^ takes the place of the import we were using previously, so we can delete it!
+// import { todos } from '../data/todo-data.js'
+
+// notice the use of async!
+async function index(req, res) {
+  try {
+    // use the model to find all todo docs
+    // await is used to stop execution until
+    // the operation is complete
+    const todos = await Todo.find({})
+  
+    res.render('todos/index', {
+      todos
+    })
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
 }
 
 export {
--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import * as todosCtrl from '../controllers/todos.js'
 const router = Router()
 
-// GET localhost:3000/users
+// GET localhost:3000/todos
 router.get('/', todosCtrl.index)
 
 export { router }
